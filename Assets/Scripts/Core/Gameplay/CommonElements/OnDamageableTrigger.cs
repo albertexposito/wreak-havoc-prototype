f299048343e11542e28c375238920b52be366471
Albert Expósito <albertexpositoramirez@gmail.com>
2024-02-09 17:19:54 +0100

    OnDamageableTrigger rename

--- Assets/Scripts/Core/Gameplay/CommonElements/OnDamageableTrigger.cs
+++ Assets/Scripts/Core/Gameplay/CommonElements/OnDamageableTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class OnDamageableTriggerEnter : MonoBehaviour
+public class OnDamageableTrigger : MonoBehaviour
 {
 
     public UnityEvent<DamageableElement> OnDamageableEnterTrigger;
